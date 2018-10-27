ca193186a11ba3a8de60e11936382eb52b51dd9e
Aziz Latipov <vm06lau@yandex-team.ru>
2018-10-27 13:06:28 +0300

    [setup] added groups for UI, Config, Resources; removed filename lines from headers; fixed codestyle

--- Todolist/ViewController.h
+++ Todolist/ViewController.h
@@ -1,15 +1,11 @@
 //
-//  ViewController.h
-//  Todolist
-//
 //  Created by Aziz Latipov on 27/10/2018.
 //  Copyright © 2018 ProjectN. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+
 @interface ViewController : UIViewController
-
-
 @end
 
--- Todolist/ViewController.m
+++ Todolist/ViewController.m
@@ -1,23 +1,14 @@
 //
-//  ViewController.m
-//  Todolist
-//
 //  Created by Aziz Latipov on 27/10/2018.
 //  Copyright © 2018 ProjectN. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface ViewController ()
 @end
 
+
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
 @end
